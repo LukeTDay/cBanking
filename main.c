411e1a79059b1977df116ef3c59a1752b87610ce
Permabannedrotmg <luke.day@gmail.com>
2025-08-09 18:59:03 -0400

    started welcome screen

--- main.c
+++ main.c
@@ -15,7 +15,6 @@ int printWelcomeScreen() {
   printf("Press 3 to log out an account\n");
   printf("Press 4 to exit\n");
 
-
   do {
     printf("Enter a valid choice (1, 2, 3 or 4): ");
     scanf("%d", &choice);
@@ -31,9 +30,29 @@ int printWelcomeScreen() {
   return choice;
 }
 
+void exit() {
+  system("cls");
+  printf("Thank you for your patronage");
+  exit(0);
+}
+
 int main() {
+
   while (true) {
     int welcomeScreenChoice = printWelcomeScreen();
+    // 1- create an account 2- log into an account 3- log out of an account 4 -exit
+    if (welcomeScreenChoice == 1) {
+
+    }
+    else if (welcomeScreenChoice == 2) {
+
+    }
+    else if (welcomeScreenChoice == 3) {
+
+    }
+    else if (welcomeScreenChoice == 4) {
+      exit();
+    }
     break;
   }
   return 0;
